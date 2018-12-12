e11fc0abb8a8686b22d2a83f07fdc705c20e07aa
Helton Souza Silveira <helton.silveira@grupoltm.com.br>
2018-12-12 09:11:44 -0200

    fisrt commit

--- ByteBank.Modelos/ParceiroComercial.cs
+++ ByteBank.Modelos/ParceiroComercial.cs
@@ -9,6 +9,7 @@ namespace ByteBank.Modelos
     public class ParceiroComercial : IAutenticavel
     {
         public string Senha { get; set; }
+
         private AutenticacaoHelper _autenticacaoHelper = new AutenticacaoHelper();
 
         public bool Autenticar(string senha)
